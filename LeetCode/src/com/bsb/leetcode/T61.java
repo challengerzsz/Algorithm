6846497d50187ac1c381d3de3704eed0df45a3fc
zengshuaizhi <challengerzsz@126.com>
2020-02-05 19:11:28 +0800

    update

--- LeetCode/src/com/bsb/leetcode/T61.java
+++ LeetCode/src/com/bsb/leetcode/T61.java
@@ -23,17 +23,42 @@ public class T61 {
 //        return head;
 
         if (head == null) return null;
-        if (head.next != null) return head;
+        if (head.next == null || k == 0) return head;
 
         ListNode end = head;
         while (end.next != null) end = end.next;
         end.next = head;
-        while (k > 0) {
+        while (k >= 0) {
             head = head.next;
             end = end.next;
             k--;
         }
         end.next = null;
         return head;
+    }
+
+    // 12 1 21
+    // 12 0 12
+
+    public ListNode rotateRight2(ListNode head, int k) {
+        if (head == null) return null;
+        if (head.next == null) return head;
+        
+        ListNode end = head;
+        int n;
+        // 找链表尾的时候顺便计数
+        for(n = 1; end.next != null; n++) end = end.next;
+        // 连接成环
+        end.next = head;
+        
+        ListNode p = head;
+        // p是移动之后的链表尾
+        for (int i = 0; i < n - k % n - 1; i++) p = p.next;
+        // 因为目前链表是成环的 所以新的头节点直接是新的尾节点的next
+        ListNode resHead = p.next;
+
+        p.next = null;
+
+        return resHead;
     }
 }
