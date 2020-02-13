cecedcd066eb56aa591609544c086163d71c68c1
zengshuaizhi <challengerzsz@126.com>
2020-02-13 13:11:31 +0800

    update

--- LeetCode/src/com/bsb/leetcode/T85.java
+++ LeetCode/src/com/bsb/leetcode/T85.java
@@ -6,20 +6,28 @@ package com.bsb.leetcode;
  */
 public class T85 {
 
+    // 这题在之前也是凑数题
+    // 最大矩阵的面积其实比较容易思考
+    // 找到可以作为矩形的对角线尽头两端点 即可计算出最后的矩形面积大小
     public int maximalRectangle(char[][] matrix) {
 
         if (matrix.length == 0) return 0;
         int max = 0;
-        // 每个点对应的最大宽度
+        // dp[i][j]表示 在第i行以第j列元素为结尾 能够构成的最长边长是多少
         int[][] dp = new int[matrix.length][matrix[0].length];
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == '1') {
+                    // 如果是第0列 并且matrix[i][j] == 1那么dp[i][j] = 1
+                    // 否则的话根据上面dp数组的定义 dp[i][j] 应该是第i行加上j之后能够构成的最大矩形边长
                     dp[i][j] = j == 0 ? 1 : dp[i][j - 1] + 1;
 
+                    // 计算以matrix[i][j]为矩形的右下角点能够构成的最大矩形面积
                     int width = dp[i][j];
                     for (int k = i; k >= 0; k--) {
+                        // 这里需要考虑到第k行第j列的最长矩形边长要是最小的
+                        // 因为如果不能保证最小的话也就意味这计算出来的矩形中不可能全部为1
                         width = Math.min(width, dp[k][j]);
                         max = Math.max(max, width * (i - k + 1));
                     }
--- LeetCode/src/com/bsb/leetcode/T97.java
+++ LeetCode/src/com/bsb/leetcode/T97.java
@@ -11,11 +11,18 @@ public class T97 {
     }
 
     // 选取s1和s2的部分进行组合 和s3进行比较
+    // 这个回溯的解其实是依赖s1的前i个字符和s2的前j个字符能不能组合成s3来实现的
     public boolean helper(String s1, int i, String s2, int j, String res, String s3) {
+        // 标准回溯
         if (res.equals(s3) && i == s1.length() && j == s2.length())
             return true;
         boolean ans = false;
         if (i < s1.length())
+            // 这里对下层递归返回的结果需要进行 |=的处理 因为有的结果集不一定能满足 有的可能回溯之后会满足结果
+            // 取s1的第i个字符
+            // 向下递归
+            // 这里后来的回溯其实是不需要对res做任何操作的 因为res是保存在每一层helper方法栈中的
+            // 递归返回的时候相当于jvm自动处理了回溯删除上一步选择
             ans |= helper(s1, i + 1, s2, j, res + s1.charAt(i), s3);
         if (j < s2.length())
             ans |= helper(s1, i, s2, j + 1, res + s2.charAt(j), s3);
@@ -23,5 +30,43 @@ public class T97 {
 
     }
 
-    
+    // 根据上面的回溯思路
+    // 定义动态规划状态和dp数组含义 以及状态转移的方式
+    // 定义dp[i][j]为s1的前i个元素和s2的前j个元素能否构成s3
+    // 那么dp[s1.length][s2.length]就是最后的解
+    public boolean isInterleaveByDp(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) return false;
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        // 初始化dp数组的第一行和第一列
+        // 思考一下dp[i][j]的定义 其实就是i + j位从s1和s2中选择出来的不同的字符序列能否构成s3的前i + j位
+        // 思考一下dp[i][0]就是s1的第i个元素能否构成s3的前i + 0位
+        // 并且初始化第一行和第一列的时候一定要考虑dp[i - 1][0] 和 dp[0][j - 1]是否为true
+        // 如果前i - 1的s1不能构成s3的前i - 1位 那第i位也就不考虑了 同理s2 也就是第一行和第一列的dp填充
+        dp[0][0] = true;
+        // 这里一定要注意后面的==判断条件.. 因为dp数组的大小所以需要进行 - 1操作...
+        for (int i = 1; i <= s1.length(); i++) {
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+        }
+        // 初始化第一行
+        for (int j = 1; j <= s2.length(); j++) {
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                // dp[i][j] 就需要考虑这一步是由s1的i构成还是s2的j字符构成
+                // 如果选择s1的i的话 那么s1的i - 1必然是构成s3的i + j - 1的元素
+                // 如果选择s2的j的话 那么s2的j - 1必然是构成s3的第i + j - 1个元素
+                // 所以dp[i][j]的状态转移应该是如下的
+                dp[i][j] = (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1)) ||
+                        (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1));
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+    }
+
+    public static void main(String[] args) {
+        new T97().isInterleaveByDp("aabcc", "dbbca", "aadbbcbcac");
+    }
+
 }
